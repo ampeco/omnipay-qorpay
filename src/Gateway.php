dfe12f45e79eabaed6d7b3315bd8ecd8cf71c828
Marry <merry.akifova@ampeco.global>
2022-09-05 10:02:01 +0300

    Add card [sc-52731]

--- src/Gateway.php
+++ src/Gateway.php
@@ -8,8 +8,15 @@ class Gateway extends AbstractGateway
 {
     use CommonParameters;
 
+    public const SECURE_FORMS_URL = 'https://secure.forms.qorcommerce.io';
+
     public function getName()
     {
         return 'QorPay';
+    }
+
+    public function createCard(array $options = []): string
+    {
+        return self::SECURE_FORMS_URL . '/vault/card/token?intent=create&' . http_build_query($options);
     }
 }
