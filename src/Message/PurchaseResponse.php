a114140fdfae22c6dbbef4f5af28cf618a44b74c
petkokostov <petko.kostov@ampeco.global>
2022-09-15 18:04:45 +0300

    fix method return type

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -4,7 +4,7 @@ namespace Ampeco\OmnipayQorPay\Message;
 
 class PurchaseResponse extends Response
 {
-    public function getTransactionReference(): string
+    public function getTransactionReference(): ?string
     {
         return @$this->data['transaction_id'];
     }
